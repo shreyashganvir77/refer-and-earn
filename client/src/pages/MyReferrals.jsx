d88fa1df24fbf519052fc9e966fb9bc0cadcffc6
shreyashganvir77 <ganvirshreyash@gmail.com>
2026-01-22 18:54:39 +0530

    MyReferrals.jsx Commenting unused variables

--- client/src/pages/MyReferrals.jsx
+++ client/src/pages/MyReferrals.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../services/api';
-import { useAuth } from '../context/AuthContext';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 const statusBadge = (status) => {
